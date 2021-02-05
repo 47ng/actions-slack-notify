6d1eba36808ef86953132b499142e305dd0096e3
Francois Best <francois@francoisbest.com>
2021-02-05 19:49:23 +0100

    chore: Connect webhook

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import * as core from '@actions/core'
 import { GitHubActionsEnv } from './gha'
 import { failure, success } from './slack'
+import { IncomingWebhook } from '@slack/webhook'
 
 async function run(): Promise<void> {
   try {
+    const url = process.env.SLACK_WEBHOOK_URL
+    if (!url) {
+      throw new Error('Missing SLACK_WEBHOOK_URL environment variable')
+    }
+    const webhook = new IncomingWebhook(url)
     const status = core.getInput('status') as
       | 'success'
       | 'failure'
@@ -12,9 +18,11 @@ async function run(): Promise<void> {
     if (status === 'success') {
       const msg = success(env)
       core.debug(msg)
+      await webhook.send(msg)
     } else if (status === 'failure') {
       const msg = failure(env, JSON.parse(core.getInput('steps')))
       core.debug(msg)
+      await webhook.send(msg)
     }
   } catch (error) {
     core.setFailed(error.message)
